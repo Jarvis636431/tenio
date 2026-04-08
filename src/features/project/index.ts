fb59e0c40e7d9076a16a067b41524ef52fa9d8a1
mengda.6364 <mengda.6364@jd.com>
2026-04-08 17:45:39 +0800

    fix: 组织架构调整

--- src/features/project/index.ts
+++ src/features/project/index.ts
@@ -1,6 +1,8 @@
 // Project Feature Module
 // 项目相关功能统一导出
 
+export { Overview } from "./pages/Overview";
+
 export { useProject } from "./hooks/useProject";
 export { useProjectCoreGraph } from "./hooks/useProjectCoreGraph";
 export { useProjectHighlight } from "./hooks/useProjectHighlight";
